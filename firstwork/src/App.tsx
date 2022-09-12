b1f10be7b32bf331bb71ed2c3f93acb862325953
FuriKuri <277541105@qq.cpom>
2022-09-12 18:29:16 +0800

    init

---
+++ firstwork/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import './utils/globalfunction'
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
