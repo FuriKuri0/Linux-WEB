ae9b9d6f75f04c51a924803a0ba71a0a8074f518
FuriKuri <277541105@qq.cpom>
2022-09-17 17:09:50 +0800

    haoe

--- firstwork/src/component/Select/index.tsx
+++ firstwork/src/component/Select/index.tsx
@@ -2,8 +2,8 @@ import { Select } from 'antd';
 import React from 'react';
 import './index.css'
 const { Option } = Select;
-type Props = { option: Array<string>, fontSize?: string, setType: Function }
-export default function Index({ option, fontSize = '16px', setType }: Props) {
+type Props = { option: Array<string>, setType: Function }
+export default function Index({ option, setType }: Props) {
 
     const handleChange = (v: number) => {
         console.log(v, 'select');
@@ -15,7 +15,7 @@ export default function Index({ option, fontSize = '16px', setType }: Props) {
             defaultValue={1}
             style={{ color: 'rgba(0,0,0,.3)', width: '90%', marginTop: '14px', marginLeft: '50%', transform: 'translateX(-50%)', }}
             dropdownStyle={{
-                fontSize
+                height: '15vh',
             }}
             onChange={(v) => handleChange(v)}
         >{option.map((v, i) => {
