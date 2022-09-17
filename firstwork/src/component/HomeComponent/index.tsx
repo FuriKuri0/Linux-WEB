0b2fba30b54c3831d0240704caa5ed8fa7af2ed3
FuriKuri <277541105@qq.cpom>
2022-09-17 10:48:41 +0800

    ycd

--- firstwork/src/component/HomeComponent/index.tsx
+++ firstwork/src/component/HomeComponent/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './index.css'
-type Props = { main: boolean }
-export default function HomeComponent({ main }: Props) {
+type Props = { where: number, which: string, data: Array<object> }
+export default function HomeComponent({ where, which, data }: Props) {
 
     return (
-        <div className={main ? 'HomeComponent' : 'HomeComponent fuzhuRight'}></div>
+        <div className={where === 0 ? 'HomeComponent' : where === 1 ? 'HomeComponent fuzhu fuzhuRight' : 'HomeComponent fuzhu fuzhuLeft'}>
+            <div className='title'>{which}</div>
+            <ul>{data.map((e) => (
+                <li><div>{Object.keys(e)[0]}</div><div>{Object.values(e)[0]}</div></li>
+            ))}
+            </ul>
+        </div>
     )
 }
--- firstwork/src/pages/Home/index.tsx
+++ firstwork/src/pages/Home/index.tsx
@@ -2,12 +2,24 @@ import React from 'react'
 import './index.css'
 import HomeComponent from '../../component/HomeComponent'
 export default function Home() {
+    const [count, setCount] = React.useState(1)
+    const next = () => {
+        setCount(count !== 1 ? count + 1 : -1)
+    }
+    const last = () => {
+        setCount(count !== -1 ? count - 1 : 1)
+    }
+    const setArr = [{ 'A相电流(A):': '-1.$' }, { 'B相电流(A):': '-1.$' }, { 'C相电流(A):': '-1.$' }, { '无功(kVar):': '0.0' },]
+    const fuArr = [{ '电流(A):': '0.0' }, { '畸变率Thdi:': '0.0%' }, { '功因(DPF)': '0.00' }, { '无功(kVar):': '0.0' },]
+    const eleArr = [{ '电流(A):': '0.0' }, { '畸变率Thdi:': '0.0%' }, { '功因(DPF)': '0.00' }, { '无功(kVar):': '0.0' },]
     return (
         <div className='Home'>
             <div className='border'></div>
-            <HomeComponent main={false} />
-            {/* <HomeComponent />
-            <HomeComponent /> */}
+            <HomeComponent where={count} which='电网' data={eleArr} />
+            <HomeComponent where={count === 0 ? 1 : count === 1 ? -1 : 0} which='设备' data={setArr} />
+            <HomeComponent where={count === 0 ? -1 : count === 1 ? 0 : 1} which='负载' data={fuArr} />
+            <button className='changeBtn' onClick={next}><img src="https://img.js.design/assets/img/63228fe609231791d1bb5790.png#ef3760511e290f0ed3ed3b328a5f4d77" alt="" /></button>
+            <button className='changeBtn' onClick={last}><img style={{ transform: 'rotateY(180deg)' }} src="https://img.js.design/assets/img/63228fe609231791d1bb5790.png#ef3760511e290f0ed3ed3b328a5f4d77" alt="" /></button>
         </div>
     )
 }
