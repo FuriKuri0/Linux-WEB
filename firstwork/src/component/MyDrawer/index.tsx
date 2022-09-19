d20aa9d08c290b03b9c86b4f2b393e6beedea74d
kkt <670335957@qq.com>
2022-09-19 19:15:27 +0800

    button

--- firstwork/src/component/MyDrawer/index.tsx
+++ firstwork/src/component/MyDrawer/index.tsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import { Button, Drawer, Radio, RadioChangeEvent, Space } from 'antd';
+import React, { useState } from 'react';
 import './index.css'
 
-export default function MyDrawer() {
+type Props={ title?:string, content?:string }
+
+const MyDrawer = () => {
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className='MyDrawer'>MyDrawer</div>
-  )
-}
+    <>
+      <Space>
+        <Button type="primary" onClick={showDrawer}>
+          Open
+        </Button>
+      </Space>
+      <Drawer
+        className='MyDrawer'
+        title={<span className='title'>参数复制</span>}
+        placement='bottom'
+        height={300}
+        closable={false}
+        onClose={onClose}
+        open={open}
+        footer={
+            <>
+                <Button type='primary' className='confirm' onClick={onClose}>确定</Button>
+                <Button type='default' className='cancle' onClick={onClose}>取消</Button>
+            </>
+        }
+      >
+        <div className='content'>确认要拷贝参数吗?</div>
+      </Drawer>
+    </>
+  );
+};
+
+export default MyDrawer;
