0e485a88956927549bddc9104176399faa7dd09a
FuriKuri <277541105@qq.cpom>
2022-09-27 19:11:49 +0800

    9.27

--- firstwork/src/pages/Chart/index.tsx
+++ firstwork/src/pages/Chart/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import WaveChart from '../../component/WaveChart'
+import Histogram from '../../component/Histogram'
+
 import './index.scss'
 export default function Chart() {
     const [which, setWhich] = React.useState(1)
@@ -7,7 +9,7 @@ export default function Chart() {
         <div className='Chart'>
             <div style={{ height: '5vh', width: '100%', background: 'white', marginBottom: '10px' }}><i onClick={() => setWhich(0)}>柱状图</i>&nbsp;&nbsp;&nbsp; / <i onClick={() => setWhich(1)}>波形图</i></div>
 
-            {which === 1 ? <WaveChart /> : ''}
+            {which === 1 ? <WaveChart /> : <Histogram />}
         </div>
     )
 }
